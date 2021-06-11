de548a59ce4268a34cbbd75642d995123802e5eb
Andrew <https://github.com/AndreyTantsiura>
2021-06-11 21:41:15 +0300

    homework_#04

--- homework_04/script.js
+++ homework_04/script.js
@@ -6,12 +6,76 @@ const themes = [
 ];
 const marks = [4, 5, 5, 3, 4, 5];
 
-function pairIntoTeams(students) {
-  let studentsTeam = [];
-  for (let i = 0; i < students.length; i++) {
-    studentsTeam.push(students[0])
-  }
-  console.log(studentsTeam);
+// #1 Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. У вас повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..], [...]];
+
+function pairIntoTeams() {
+  const studentsTeams = [];
+
+  const firstTeam = [].concat(students[0], students[2]);
+  const secondTeam = [].concat(students[1], students[3]);
+  const thirdTeam = [].concat(students[0], students[2]);
+  studentsTeams.push(firstTeam, secondTeam, thirdTeam);
+
+  return studentsTeams;
 }
 
-pairIntoTeams(students);
+console.log(pairIntoTeams());
+
+// #2 Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
+
+function teamsConcatWithThemes() {
+  const teamsConcatThemes = [];
+
+  const firstTeamThemes = [].concat(pairIntoTeams()[0].join(" i "), themes[0]);
+  const secondTeamThemes = [].concat(pairIntoTeams()[1].join(" i "), themes[1]);
+  const thirdTeamThemes = [].concat(pairIntoTeams()[2].join(" i "), themes[2]);
+  teamsConcatThemes.push(firstTeamThemes, secondTeamThemes, thirdTeamThemes);
+
+  return teamsConcatThemes;
+}
+
+console.log(teamsConcatWithThemes());
+
+// #3 Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
+
+function studentsConcatWithMarks() {
+  const studentsMarks = [];
+  for (let i = 0; i < students.length; i++) {
+    studentsMarks.push(students[i] + ", " + marks[i]);
+  }
+  return studentsMarks;
+}
+
+console.log(studentsConcatWithMarks());
+
+// #4 Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт(тут функція буде нечистою, але не повинна мутувати массив): [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]]
+
+function teamsThemesConcatMarks() {
+  const teamsThemesMarks = [];
+
+  const firstRandomMark = marks[Math.floor(Math.random() * marks.length)];
+  const secondRandomMark = marks[Math.floor(Math.random() * marks.length)];
+  const thirdRandomMark = marks[Math.floor(Math.random() * marks.length)];
+
+  const firstTeamsThemesMarks = [].concat(
+    teamsConcatWithThemes()[0],
+    firstRandomMark
+  );
+  const secondTeamsThemesMarks = [].concat(
+    teamsConcatWithThemes()[1],
+    secondRandomMark
+  );
+  const thirdTeamsThemesMarks = [].concat(
+    teamsConcatWithThemes()[2],
+    thirdRandomMark
+  );
+  teamsThemesMarks.push(
+    firstTeamsThemesMarks,
+    secondTeamsThemesMarks,
+    thirdTeamsThemesMarks
+  );
+
+  return teamsThemesMarks;
+}
+
+console.log(teamsThemesConcatMarks());
