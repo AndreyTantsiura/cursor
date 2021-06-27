b52c3d3a474e9e2d84930291dbaa199cdb5bd217
Andrew <https://github.com/AndreyTantsiura>
2021-06-27 18:59:52 +0300

    add homework_08

--- homework_08/script.js
+++ homework_08/script.js
@@ -1,25 +1,36 @@
 class Student {
-  constructor(university, course, fullName, marks) {
+  constructor(university, course, fullName) {
     this.university = university;
     this.course = course;
     this.fullName = fullName;
-    this.marks = marks;
+    this.marks = [5, 4, 4, 5];
   }
 
   getInfo() {
+    if (!this.university) return null;
     return `Студент ${this.course}го курсу ${this.university}, ${this.fullName}`;
   }
 
   get getMarks() {
+    if (!this.university) return null;
     return this.marks;
   }
 
-  set setMarks(mark) {
-    return this.marks.push(mark);
+  set setMarks(newMark) {
+    if (!this.university) return null;
+    return this.marks.push(newMark);
   }
 
   getAverageMark() {
     return this.marks.reduce((a, b) => a + b) / this.marks.length;
+  }
+
+  dismiss() {
+    this.university = !this.university;
+  }
+
+  recover() {
+    this.university = true;
   }
 }
 
@@ -27,8 +38,7 @@ class Student {
 const ostap = new Student(
   "Вищої Школи Психотерапії м.Одеса",
   "1",
-  "Остап Родоманський Бендер",
-  [5, 4, 4, 5]
+  "Остап Родоманський Бендер"
 );
 
 //2
@@ -43,3 +53,11 @@ console.log("Оцінки студента після оцінювання:", os
 
 //5
 console.log("Cередній бал:", ostap.getAverageMark());
+
+//6
+ostap.dismiss();
+console.log("Оцінки студента до оцінювання:", ostap.getMarks);
+
+//7
+ostap.recover();
+console.log("Оцінки студента до оцінювання:", ostap.getMarks);
