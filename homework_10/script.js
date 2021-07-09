7b41df275b0b765acb40743df0adbb43532f28eb
Andrew <https://github.com/AndreyTantsiura>
2021-07-09 07:29:07 +0300

    add to peer-review

--- homework_10/script.js
+++ homework_10/script.js
@@ -1,6 +1,16 @@
 const keys = document.querySelector(".keys");
-const audio = document.querySelectorAll("audio");
 const key = keys.querySelectorAll(".key");
+const audio = document.querySelectorAll("audio");
+
+document.addEventListener("keydown", (e) => {
+  removeClassPlaying();
+  audio.forEach((item) => {
+    if (item.getAttribute("id") === e.code) {
+      item.play();
+      keys.querySelector(`#${e.code}`).classList.add("playing");
+    }
+  });
+});
 
 keys.addEventListener("click", (e) => {
   removeClassPlaying();
@@ -15,5 +25,9 @@ keys.addEventListener("click", (e) => {
 function removeClassPlaying() {
   key.forEach((item) => {
     item.classList.remove("playing");
+  });
+  audio.forEach((item) => {
+    item.pause();
+    item.currentTime = 0;
   });
 }
