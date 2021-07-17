68282941da295e44dee604b309f5c782813db1e4
Andrew <https://github.com/AndreyTantsiura>
2021-07-17 21:52:34 +0300

    add himework_13

--- homework_13/script.js
+++ homework_13/script.js
@@ -3,20 +3,29 @@ const getCharacters = document.getElementById("getCharacters");
 getCharacters.addEventListener("click", getCharactersData);
 
 function getCharactersData() {
+  document.getElementById("planetsList").innerHTML = "";
   axios.get("https://swapi.dev/api/films/2/").then((respons) => {
     arrLink = respons.data.characters;
     arrLink.map((item) => {
       return axios.get(item).then((response) => {
+        const id = parseInt(response.config.url.replace(/\D+/g, ""));
+
+        const img = document.createElement("img");
+        img.setAttribute("src", `./img/${id}.png`);
+
         const result = {
           name: response.data.name,
           birth_year: response.data.birth_year,
           gender: response.data.gender,
         };
+
         const charactersList = document.getElementById("characters");
+
         charactersList.insertAdjacentHTML(
           "beforeend",
-          `<div id="icon">${result.name} (${result.birth_year}, ${result.gender})</div>`
+          `<div id="icon"><p>${result.name} (${result.birth_year}, ${result.gender})</p></div>`
         );
+        charactersList.append(img);
       });
     });
   });
@@ -28,6 +37,7 @@ getPlanetsList.addEventListener("click", getPlanets);
 
 let planets = null;
 function getPlanets() {
+  document.getElementById("characters").innerHTML = "";
   axios.get("https://swapi.dev/api/planets/").then((response) => {
     planets = response.data.results;
     sortPlanets();
