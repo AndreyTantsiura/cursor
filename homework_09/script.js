10cb5141bc98700499e0586e54974bf9d465bfb2
Andrew <https://github.com/AndreyTantsiura>
2021-07-19 01:50:49 +0300

    refactoring done

--- homework_09/script.js
+++ homework_09/script.js
@@ -1,28 +1,31 @@
-function generateBlocks(width = "50", height = "50", column = 5) {
+function generateBlocks(blockWidth = "50", blockHeidht = "50", column = 5) {
   const row = document.createElement("div");
   row.setAttribute("id", "row");
-  row.style.width = width * column + "px";
+  row.style.width = blockWidth * column + "px";
   document.body.append(row);
 
   for (let i = 0; i < 25; i++) {
     const block = document.createElement("div");
     block.classList.add("block");
-    block.style.width = width + "px";
-    block.style.height = height + "px";
-    block.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    block.style.width = blockWidth + "px";
+    block.style.height = blockHeidht + "px";
+    block.style.backgroundColor = `getRandomColor()`
     row.append(block);
   }
 }
 
 generateBlocks();
 
+const getRandomColor = () => {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor
+}
+
 function generateBlocksInterval() {
   setInterval(() => {
-    const block = document.querySelectorAll(".block");
-    block.forEach((item) => {
-      item.style.backgroundColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((item) => {
+      item.style.backgroundColor = getRandomColor()
     });
   }, 1000);
 }
