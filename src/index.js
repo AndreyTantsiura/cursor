9e39f899041f9564827ff4d83dfcd9bb9e71f0fb
Andrew <https://github.com/AndreyTantsiura>
2021-07-26 22:47:33 +0300

    create hw#16

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import {
   getRandomChinese,
 } from "./modules.js";
 
-document.body.innerHTML = `HW#1. Максимальне число = ${getMaxPrice()};<br>
+document.body.innerHTML = `
+HW#1. Максимальне число = ${getMaxPrice()};<br>
 HW#3. Функція №1: ${getMaxDigit(-1278)};<br>
 HW#4. Function 1: ${pairIntoTeams()};<br>
 HW#5. 1) array: ${getRandomArray(15, 1, 100)};<br>
@@ -21,7 +22,5 @@ HW#6. List subjects: ${getSubjects()};<br>
 HW#7. ${getMiddleTaxes.call(ukraine, "ukraine")};<br>
 HW#8. Дані студента: ${ostap.getInfo(Student)};<br>
 HW#9. Random color: ${getRandomColor()};<br>
-HW#11. ${getRandomChinese(4).then((result) => {
-  document.body.append(result);
-})};<br>
+HW#11. ${getRandomChinese(4).then(console.log)};<br>
 `;
